ce39194c14d1b9fc3c71f43e81fcfc3221c3dac7
Spratch <contact@josephclenet.fr>
2024-12-20 15:15:45 +0100

    feat(dashboard): score graph

--- src/components/dashboard/Score/index.tsx
+++ src/components/dashboard/Score/index.tsx
@@ -1,4 +1,6 @@
+import "./score.scss";
 import { UserType } from "../../../types/user";
+import { Pie, PieChart, ResponsiveContainer } from "recharts";
 
 type Props = {
   score: UserType["todayScore"];
@@ -7,9 +9,45 @@ type Props = {
 export default function Score({ score }: Props) {
   return (
     <article className="dashboard__chart dashboard__chart--score">
-      <h2>score</h2>
-      <p>
-        <span className="">{score * 100}%</span> de votre objectif
+      <h2 className="score__title">Score</h2>
+
+      {/* Arc graph */}
+      <ResponsiveContainer
+        width="100%"
+        height="100%"
+      >
+        <PieChart
+          width={400}
+          height={400}
+        >
+          <Pie
+            data={[{ value: 1 }]}
+            dataKey="value"
+            outerRadius={70}
+            fill="white"
+            stroke="none"
+            isAnimationActive={false}
+          />
+          <Pie
+            data={[
+              { value: score * 100, fill: "#FF0000", stroke: "none" },
+              { value: 100 - score * 100, fill: "none", stroke: "none" }
+            ]}
+            dataKey="value"
+            nameKey="name"
+            innerRadius={70}
+            outerRadius={80}
+            startAngle={90}
+            cornerRadius={50}
+          />
+        </PieChart>
+      </ResponsiveContainer>
+
+      {/* Score percentage */}
+      <p className="score__percentage">
+        <span>{score * 100}%</span>
+        <br />
+        de votre objectif
       </p>
     </article>
   );
