752a5b658de45f21f65fdde3191f571b653102ec
Spratch <contact@josephclenet.fr>
2024-12-27 16:24:05 +0100

    feat(dashboard): activity graph

--- src/components/dashboard/Activity/index.tsx
+++ src/components/dashboard/Activity/index.tsx
@@ -1,3 +1,14 @@
+import "./activity.scss";
+import {
+  Bar,
+  BarChart,
+  CartesianGrid,
+  Legend,
+  ResponsiveContainer,
+  Tooltip,
+  XAxis,
+  YAxis
+} from "recharts";
 import { ActivityType } from "../../../types/activity";
 
 type Props = {
@@ -7,22 +18,75 @@ type Props = {
 export default function Activity({ activity }: Props) {
   return (
     <article className="dashboard__chart dashboard__chart--activity">
+      {/* Title */}
       <h2>Activité quotidienne</h2>
-      <div className="chart__legend">
-        <div className="legend__item legend__item--weight">Poids (kg)</div>
-        <div className="legend__item legend__item--burned">
-          Calories brûlées (kCal)
-        </div>
-      </div>
-      {activity.sessions.map((session) => (
-        <div
-          key={session.day}
-          className="chart__bar"
+
+      {/* Chart */}
+      <ResponsiveContainer
+        width="100%"
+        height="100%"
+      >
+        <BarChart
+          data={activity.sessions}
+          barSize={7}
+          barGap={8}
         >
-          <div className="chart__bar__weight">{session.kilogram}kg</div>
-          <div className="chart__bar__burned">{session.calories}kCal</div>
-        </div>
-      ))}
+          <XAxis
+            dataKey="day"
+            tickLine={false}
+            tickMargin={16}
+            tickFormatter={(day) => new Date(day).getDate().toString()}
+            tick={{ fill: "#9B9EAC" }}
+          />
+          <YAxis
+            yAxisId="right"
+            orientation="right"
+            axisLine={false}
+            tickLine={false}
+            domain={["dataMin - 2", "dataMax + 1"]}
+            tickCount={3}
+            tickMargin={35}
+            tick={{ fill: "#9B9EAC" }}
+          />
+          <YAxis
+            yAxisId="left"
+            hide
+          />
+
+          <CartesianGrid
+            stroke="#DEDEDE"
+            vertical={false}
+            strokeDasharray={"2"}
+          />
+          <Tooltip />
+          <Bar
+            dataKey="kilogram"
+            name="Poids (kg)"
+            fill="#282D30"
+            yAxisId="right"
+            radius={[3, 3, 0, 0]}
+          />
+          <Bar
+            dataKey="calories"
+            name="Calories brûlées (kCal)"
+            fill="#E60000"
+            yAxisId="left"
+            radius={[3, 3, 0, 0]}
+          />
+          <Legend
+            verticalAlign="top"
+            align="right"
+            iconType="circle"
+            iconSize={9}
+            height={50}
+            wrapperStyle={{
+              top: 0,
+              right: 0,
+              fontSize: "0.875rem"
+            }}
+          />
+        </BarChart>
+      </ResponsiveContainer>
     </article>
   );
 }
